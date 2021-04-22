a28fc4ee5270b70e72d8ffbbf1e66c21df711cef
planetis-m <b3lievem@yandex.com>
2021-04-22 19:38:29 +0300

    comments who needs them amrite?

--- sync.nim
+++ sync.nim
@@ -7,19 +7,12 @@ type
 
 proc acquire*(s: var SpinLock) =
   while true:
-    # Optimistically assume the lock is free on the first try.
     if not atomicExchangeN(addr s.lock, true, AtomicAcquire)
       return
     else:
-      # Wait for lock to be released without generating cache misses.
-      while atomicLoadN(addr s.lock, AtomicRelaxed):
-        # Reduces contention between hyper-threads.
-        cpuRelax()
+      while atomicLoadN(addr s.lock, AtomicRelaxed): cpuRelax()
 
 proc tryAcquire*(s: var SpinLock): bool =
-  var oldLock = false
-  # First do a relaxed load to check if lock is free in order to prevent
-  # unnecessary cache misses if someone does `while not tryAcquire(s)`
   result = not atomicLoadN(addr s.lock, AtomicRelaxed) and
       not atomicExchangeN(addr s.lock, true, AtomicAcquire)
 
