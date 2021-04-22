c71367ec3464beac394bef1e99d2a3abc15cec97
planetis-m <b3lievem@yandex.com>
2021-04-22 19:36:34 +0300

    first

---
+++ sync.nim
@@ -0,0 +1,156 @@
+import std / locks
+{.push stackTrace: off.}
+
+type
+  SpinLock* = object
+    lock: bool
+
+proc acquire*(s: var SpinLock) =
+  while true:
+    # Optimistically assume the lock is free on the first try.
+    if not atomicExchangeN(addr s.lock, true, AtomicAcquire)
+      return
+    else:
+      # Wait for lock to be released without generating cache misses.
+      while atomicLoadN(addr s.lock, AtomicRelaxed):
+        # Reduces contention between hyper-threads.
+        cpuRelax()
+
+proc tryAcquire*(s: var SpinLock): bool =
+  var oldLock = false
+  # First do a relaxed load to check if lock is free in order to prevent
+  # unnecessary cache misses if someone does `while not tryAcquire(s)`
+  result = not atomicLoadN(addr s.lock, AtomicRelaxed) and
+      not atomicExchangeN(addr s.lock, true, AtomicAcquire)
+
+proc release*(s: var SpinLock) =
+  atomicStoreN(addr s.lock, false, AtomicRelease)
+
+template withSpinLock*(a: SpinLock, body: untyped) =
+  acquire(a)
+  try:
+    body
+  finally:
+    release(a)
+
+type
+  Semaphore* = object
+    c: Cond
+    L: Lock
+    counter: int
+
+proc initSemaphore*(s: var Semaphore; value: Natural = 0) =
+  initCond(s.c)
+  initLock(s.L)
+  s.counter = value
+
+proc destroySemaphore*(s: var Semaphore) {.inline.} =
+  deinitCond(s.c)
+  deinitLock(s.L)
+
+proc blockUntil*(s: var Semaphore) =
+  acquire(s.L)
+  while s.counter <= 0:
+    wait(s.c, s.L)
+  dec s.counter
+  release(s.L)
+
+proc signal*(s: var Semaphore) =
+  acquire(s.L)
+  inc s.counter
+  signal(s.c)
+  release(s.L)
+
+type
+  Barrier* = object
+    c: Cond
+    L: Lock
+    required: int # number of threads needed for the barrier to continue
+    left: int # current barrier count, number of threads still needed.
+    cycle: uint # generation count
+
+proc initBarrier*(b: var Barrier; count: Natural) =
+  b.required = count
+  b.left = count
+  b.cycle = 0
+  initCond(b.c)
+  initLock(b.L)
+
+proc destroyBarrier*(b: var Barrier) {.inline.} =
+  deinitCond(b.c)
+  deinitLock(b.L)
+
+proc wait*(b: var Barrier) =
+  acquire(b.L)
+  dec b.left
+  if b.left == 0:
+    inc b.cycle
+    b.left = b.required
+    broadcast(b.c)
+  else:
+    let cycle = b.cycle
+    while cycle == b.cycle:
+      wait(b.c, b.L)
+  release(b.L)
+
+type
+  RwMonitor* = object
+    readPhase: Cond
+    writePhase: Cond
+    L: Lock
+    counter: int # can be in three states: free = 0, reading > 0, writing = -1
+
+proc initRwMonitor*(rw: var RwMonitor) =
+  initCond rw.readPhase
+  initCond rw.writePhase
+  initLock rw.L
+  rw.counter = 0
+
+proc destroyRwMonitor*(rw: var RwMonitor) {.inline.} =
+  deinitCond(rw.readPhase)
+  deinitCond(rw.writePhase)
+  deinitLock(rw.L)
+
+proc beginRead*(rw: var RwMonitor) =
+  acquire(rw.L)
+  while rw.counter == -1:
+    wait(rw.readPhase, rw.L)
+  inc rw.counter
+  release(rw.L)
+
+proc beginWrite*(rw: var RwMonitor) =
+  acquire(rw.L)
+  while rw.counter != 0:
+    wait(rw.writePhase, rw.L)
+  rw.counter = -1
+  release(rw.L)
+
+proc endRead*(rw: var RwMonitor) =
+  acquire(rw.L)
+  dec rw.counter
+  if rw.counter == 0:
+    rw.writePhase.signal()
+  release(rw.L)
+
+proc endWrite*(rw: var RwMonitor) =
+  acquire(rw.L)
+  rw.counter = 0
+  rw.readPhase.broadcast()
+  rw.writePhase.signal()
+  release(rw.L)
+
+template readWith*(a: RwMonitor, body: untyped) =
+  beginRead(a)
+  try:
+    body
+  finally:
+    endRead(a)
+
+template writeWith*(a: RwMonitor, body: untyped) =
+  beginWrite(a)
+  try:
+    body
+  finally:
+    endWrite(a)
+
+{.pop.}
