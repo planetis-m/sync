634d0082ebafb0fec87265a371cd36d595ee3e8c
planetis-m <b3lievem@yandex.com>
2021-04-23 16:43:10 +0300

    added simple once based on locks

--- sync.nim
+++ sync.nim
@@ -68,6 +68,30 @@ template withLock*(a: SpinLock, body: untyped) =
     release(a)
 
 type
+  Once* = object
+    L: Lock
+    finished: bool
+
+proc initOnce*(o: var Once) =
+  initLock(o.L)
+  o.finished = false
+
+proc destroyOnce*(o: var Once) {.inline.} =
+  deinitLock(o.L)
+
+template once*(o: Once, body: untyped) =
+  if not atomicLoadN(addr o.finished, AtomicAcquire):
+    acquire o.L
+    try:
+      if not o.finished:
+        try:
+          body
+        finally:
+          atomicStoreN(addr o.finished, true, AtomicRelease)
+    finally:
+      release o.L
+
+type
   Semaphore* = object
     c: Cond
     L: Lock
