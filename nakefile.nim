c9811af2d03dc78a920d33592d0a3daae3d9e68f
planetis-m <b3lievem@yandex.com>
2021-06-29 11:03:57 +0300

    added docs

---
+++ nakefile.nim
@@ -0,0 +1,29 @@
+import nake, std/strformat
+
+task "docs", "Generate documentation":
+  # https://nim-lang.github.io/Nim/docgen.html
+  let
+    sync = "sync"
+    src = [
+      sync.addFileExt(".nim"),
+      #sync / "barrier.nim", sync / "once.nim",
+      #sync / "rwlock.nim", sync / "semaphore.nim",
+      #sync / "spinlock.nim"
+    ]
+    dir = "docs/"
+    doc = dir / sync.addFileExt(".html")
+    url = "https://github.com/planetis-m/sync"
+  if doc.needsRefresh(src):
+    echo "Generating the docs..."
+    direShell(nimExe,
+        &"doc --threads:on --project --verbosity:0 --git.url:{url} --git.devel:master --git.commit:master --out:{dir} {src[0]}")
+    withDir(dir):
+      moveFile("theindex.html", "index.html")
+  else:
+    echo "Skipped generating the docs."
+
+task "test", "Run the tests":
+  withDir("tests/"):
+    for f in walkFiles("t*.nim"):
+      echo "Running test ", f, "..."
+      direShell(nimExe, &"c -r --threads:on --hints:off -w:off --path:../ {f}")
